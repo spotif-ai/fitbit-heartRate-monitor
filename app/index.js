22228c35f17aaf546557bb8a632b167d8aa27935
Keerat Grewal <keeratgrewal@Keerats-MacBook-Pro.local>
2019-02-03 05:49:21 -0800

    added firebase imports

--- app/index.js
+++ app/index.js
@@ -5,6 +5,34 @@ import * as util from "../common/utils";
 import { HeartRateSensor } from "heart-rate";
 import { BodyPresenceSensor } from "body-presence";
 import { me } from "appbit";
+import firebase from "firebase";
+import 'firebase/storage';
+
+
+
+const myLabel = document.getElementById("myLabel");
+
+var config = {
+    apiKey: "AIzaSyAMvAZt36acSjcZAh5MsIcTinA6fZSh5cE",
+    authDomain: "spotif-ai-3c5c6.firebaseapp.com",
+    databaseURL: "https://spotif-ai-3c5c6-35a1b.firebaseio.com",
+    projectId: "spotif-ai-3c5c6",
+    storageBucket: "spotif-ai-3c5c6-35a1b.appspot.com",
+    messagingSenderId: "38521654950"
+ };
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyAMvAZt36acSjcZAh5MsIcTinA6fZSh5cE",
+    authDomain: "spotif-ai-3c5c6.firebaseapp.com",
+    databaseURL: "https://spotif-ai-3c5c6-35a1b.firebaseio.com",
+    projectId: "spotif-ai-3c5c6",
+    storageBucket: "spotif-ai-3c5c6-35a1b.appspot.com",
+    messagingSenderId: "38521654950"
+});
+
+var database = firebase.database();
+
+
 
 // Update the clock every minute
 clock.granularity = "minutes";
@@ -32,6 +60,13 @@ bodysensor.onreading = () =>{
 heartRateSensor.onreading = function() {
   console.log("Current heart rate: " + heartRateSensor.heartRate);
   myLabel.text = "HR: " + heartRateSensor.heartRate;
+  writeUserData(heartRateSensor.heartRate)
+}
+
+function writeUserData(heartRate) {
+  firebase.database().ref('heartRate/').set({
+    heartrate: heartRate
+ });
 }
 
 bodysensor.start();
